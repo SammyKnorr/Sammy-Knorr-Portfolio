d3fe1b2cfedf211a599aaa3bb93ee770bf0d2344
Sammy Knorr <jsm8st@virginia.edu>
2023-11-21 14:31:42 +0100

    First Commit

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -40,36 +40,21 @@ const Contact = () => {
 
 					<div className="contact-container">
 						<div className="title contact-title">
-							Let's Get in Touch: Ways to Connect with Me
+							Ways to message me!
 						</div>
 
 						<div className="subtitle contact-subtitle">
-							Thank you for your interest in getting in touch with
-							me. I welcome your feedback, questions, and
-							suggestions. If you have a specific question or
-							comment, please feel free to email me directly at
+						If you're interested in discussing potential projects, collaborations, or 
+						just want to say hello, I'd love to hear from you! Feel free to reach out 
+						to me at
 							&nbsp;{" "}
 							<a href={`mailto:${INFO.main.email}`}>
 								{INFO.main.email}
 							</a>
-							. I make an effort to respond to all messages within
-							24 hours, although it may take me longer during busy
-							periods. Alternatively, you can use the contact form
-							on my website to get in touch. Simply fill out the
-							required fields and I'll get back to you as soon as
-							possible. Finally, if you prefer to connect on
-							social media, you can find me on{" "}
-							<a
-								href={INFO.socials.instagram}
-								target="_blank"
-								rel="noreferrer"
-							>
-								{INFO.socials.instagram}
-							</a>
-							. I post regular updates and engage with my
-							followers there, so don't hesitate to reach out.
-							Thanks again for your interest, and I look forward
-							to hearing from you!
+							&nbsp;{" "}
+						for any inquiries or professional discussions. For more insights into 
+						my work and professional network, please visit my LinkedIn and GitHub 
+						profiles linked below.{" "}
 						</div>
 					</div>
 
