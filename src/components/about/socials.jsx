f02cb0157692f58600239dd46b45184d18e69de0
Sammy Knorr <jsm8st@virginia.edu>
2024-07-08 12:18:42 -0700

    Rebuilding project

--- src/components/about/socials.jsx
+++ src/components/about/socials.jsx
@@ -58,7 +58,7 @@ const Socials = () => {
 					</a>
 				</div>
 			</div>
-			<a href={process.env.PUBLIC_URL + "/Samuel-Knorr-Resume.pdf"} download="Samuel-Knorr-Resume.pdf">Download CV</a>
+			<a href={process.env.PUBLIC_URL + "/Samuel-Knorr-Resume.pdf"} download="SKnorr-Resume.pdf">Download CV</a>
 		</div>
 	);
 };
